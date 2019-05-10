fce20332c40d25fef9115a3b5fcc88e239780e35
dujianwu123 <dujianwu123@126.com>
2019-05-10 15:40:40 +0800

    添加icon 和iconPositin 属性

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,23 @@
 <template>
-    <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
+  <!-- <button class="g-button" v-else>
+      <svg v-if="icon" class="icon">
+        <use :xlink:href="`#i-${icon}`"></use>
+      </svg>
+      <slot></slot>
+  </button>-->
 </template>
 
 <script>
 export default {
-  
+  props: ["icon", "iconPosition"]
 };
 </script>
 
@@ -16,17 +29,35 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
-
   &:active {
     background-color: var(--button-active-bg);
   }
-
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: .1em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .1em;
+    }
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
