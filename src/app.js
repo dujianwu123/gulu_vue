4357a66323b570060c85efa6bed78ae2e7dc7a71
dujianwu123 <dujianwu123@126.com>
2019-05-10 16:10:45 +0800

    把icon的svg提取成icon组件

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import Button from './button.vue';
+import Icon from './icon'
+Vue.component('g-icon', Icon)
+Vue.component('g-button', Button)
 new Vue({
-  el: '#app',
-  components: {
-    'g-button': Button
-  }
+  el: '#app'
 })
